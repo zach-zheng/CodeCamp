822b6daf944a8545bdcb99c9bfe37b4c7b4244bc
zach-zheng <zhikui.cn@msn.com>
2025-09-27 17:43:00 +0800

    trace it

--- Python/python.py
+++ Python/python.py
@@ -1,11 +1,18 @@
-filename = 'text.txt'
+filename = 'pi_digits.txt'
 
+with open(filename) as file_object:
+    for line in file_object:
+        print(line)
 
-try:
-    with open(filename) as file_object:
-        contents = file_object.read()
-except FileNotFoundError:
-    msg = "Sorry, the file " + filename + " does not exist."
-    print(msg)
+# try:
+#     with open(filename) as file_object:
+#         contents = file_object.read()
+# except FileNotFoundError:
+#     msg = "Sorry, the file " + filename + " does not exist."
+#     print(msg)
+# else:
+#     #counting number of words in file
+#     words = contents.split()
+#     num_words = len(words)
+#     print("This file " + filename + " has about " + str(num_words) + " words.")
  
-
